127a3fc6e6e38c71bbdabf66ae18ce83aa07a67a
sarita atanacio <atanacio.sarita@pucp.pe>
2020-09-29 10:33:24 -0500

    Merge branch 'master' of C:\Users\atana\StudioProjects\Lab2G6 with conflicts.

---
+++ app/src/main/java/com/example/tel306/AgregarUsuario.java
@@ -0,0 +1,19 @@
+package com.example.tel306;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        setTitle("Agregar usuario");
+
+        public void obtenerDatos(View view){
+        }
+    }
+}
