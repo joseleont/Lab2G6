5719fef2bf108398832755d75a5088baa9628d8e
sarita atanacio <atanacio.sarita@pucp.pe>
2020-09-29 10:39:25 -0500

    lab

--- app/src/main/java/com/example/tel306/AgregarNuevaTarea.java
+++ app/src/main/java/com/example/tel306/AgregarNuevaTarea.java
@@ -2,11 +2,13 @@ package com.example.tel306;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AgregarNuevaTarea extends AppCompatActivity {
 
